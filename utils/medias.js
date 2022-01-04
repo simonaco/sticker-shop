7bc30873fe5530d1a90dfb5e3a4fd67063411c48
Simona Cotin <simona.cotin@gmail.com>
2022-01-04 20:38:16 +0100

    update medias

--- utils/medias.js
+++ utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
-    return `http://localhost:1337${url}`
+    return `https://stickershop.azurewebsites.net${url}`
   }
   // Otherwise return full URL
   return url
