e6d4dd6e5562864eab18d66c7ddfeb75e3067b8c
Simona Cotin <simona.cotin@gmail.com>
2022-01-05 11:06:19 +0100

    add auth

---
+++ components/Logout.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="logout">Logout</button>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    logout() {
+      const { pathname } = window.location;
+      const redirect = `post_logout_redirect_uri=${pathname}`;
+      const url = `/.auth/logout?${redirect}`;
+      window.location.href = url;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,31 +1,71 @@
 <template>
-<div class="flex justify-between ml-6 mr-6 mt-4">
-  <nuxt-link to="/">
-    <span class="emoji">
-      <img src="~/assets/strapi.png" class="logo" height="150" width="150" />
-    </span>
-  </nuxt-link>
-
-  <button class="snipcart-checkout flex items-center">
-    <Cart />
-    <span class="snipcart-total-price ml-3 font-semibold text-sm text-indigo-500"></span>
-  </button>
-</div>
+  <div class="flex justify-between ml-6 mr-6 mt-4">
+    <nuxt-link to="/">
+      <span class="emoji">
+        <img src="~/assets/strapi.png" class="logo" height="150" width="150" />
+      </span>
+    </nuxt-link>
+    <div class="menu-list auth">
+      <template v-if="!userInfo">
+        <template v-for="provider in providers">
+          <Login :key="provider" :provider="provider" />
+        </template>
+      </template>
+      <Logout v-if="userInfo" />
+    </div>
+    <div v-if="userInfo">
+      <p>Welcome, {{ userInfo.userDetails }}</p>
+    </div>
+    <button class="snipcart-checkout flex items-center">
+      <Cart />
+      <span
+        class="snipcart-total-price ml-3 font-semibold text-sm text-indigo-500"
+      ></span>
+    </button>
+  </div>
 </template>
 
 <script>
-import Cart from "./icons/cart.vue"
+import Cart from "./icons/cart.vue";
+import Login from "~/components/Login.vue";
+import Logout from "~/components/Logout.vue";
 
 export default {
+  data() {
+    return {
+      userInfo: {
+        type: Object,
+        default() {},
+      },
+      providers: ["twitter", "github", "aad"],
+    };
+  },
+  async created() {
+    this.userInfo = await this.getUserInfo();
+  },
+  methods: {
+    async getUserInfo() {
+      try {
+        const response = await fetch("/.auth/me");
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        return clientPrincipal;
+      } catch (error) {
+        console.error("No profile could be found");
+        return undefined;
+      }
+    },
+  },
   components: {
-    Cart
-  }
-}
+    Cart,
+    Login,
+    Logout
+  },
+};
 </script>
 
 <style>
 .emoji {
   font-size: 30px;
 }
-
 </style>
