b98f84458d1531b927a19f28565632bd00309a63
Simona Cotin <simona.cotin@gmail.com>
2022-01-06 10:27:46 +0100

    demove loadtest

--- api/loadtest/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "loaderio-1d2d8e4a50431b29a6bc8b0b78b40c78"
-    };
-}
